886723fae4db0afbb84a0086461755ea7c92ec5a
Indrianingsihputri <indrianingsihputri30@gmail.com>
2025-03-18 19:33:07 +0700

    Pembuatan Program While

---
+++ Perulangan2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main(){
+     // Perilangan While
+     int x;
+     int PerulanganWhile = 0;
+     int Perulangan = 0;
+
+     srand(time(0));
+     x = rand() % 10;
+     cout << "Perulangan While " << endl;
+     cout << "Nilai x Awal : " << x << endl;
+    
+}
